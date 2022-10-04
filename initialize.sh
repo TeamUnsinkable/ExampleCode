21934b4a3448f1b6e6d698123232a4b082ffbd15
Connorwb <connorwb87@gmail.com>
2022-10-03 20:41:23 -0400

    Bugfixin

--- initialize.sh
+++ initialize.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
 catkin init
 catkin build
-echo 'source ~/ExampleCode/devel/setup.bash' >> ~/.bashrc
-chmod -x src/nodeexamples/scripts/pong.py
+echo "source $PWD/devel/setup.bash" >> ~/.bashrc
+chmod +x src/nodeexamples/scripts/pong.py
